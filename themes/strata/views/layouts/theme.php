eafebbfca3b634ee4b4128f8ec6247274d5402d7
boehsermoe <boehsermoe@me.com>
2020-04-17 07:54:01 +0200

    Header and Footer layouts

--- themes/strata/views/layouts/theme.php
+++ themes/strata/views/layouts/theme.php
@@ -24,19 +24,11 @@ $this->beginPage();
     </head>
     <body>
     <?php $this->beginBody() ?>
-    <?= $content ?>
 
-    <footer>
-        <nav>
-            <ul class="nav">
-                <?php foreach (Yii::$app->menu->findAll(['depth' => 1, 'container' => 'default']) as $item): /* @var $item \luya\cms\menu\Item */ ?>
-                    <li class="nav-item<?= $item->isActive ? ' active' : '' ?>">
-                        <a class="nav-link" href="<?= $item->link; ?>"><?= $item->title; ?></a>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
-        </nav>
-    </footer>
+    <?php echo $this->render('_header') ?>
+    <?= $content ?>
+    <?php echo $this->render('_footer') ?>
+
     <?php $this->endBody() ?>
     </body>
     </html>
